bc8fe2219107fef2a362234766c5dd6d0a5d59ef
ron gokhle <cips173@gmail.com>
2012-05-27 12:02:16 +0530

    update for script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#based on script found at https://github.com/alanorth/kernel_huawei_u8160/blob/cm7/build-kernel.sh
+#based on script to compile rm kernel 
 
 function setenv {
 	echo -n "Setting ARM environment..."
@@ -7,7 +7,7 @@ function setenv {
 	export USE_CCACHE=1
 
 	#THIS LINE SHOULD REFLECT YOUR PATH TO THE TOOLCHAIN!
-	export CROSS_COMPILE="/home/cips/Linaro/android-toolchain-eabi/bin/arm-eabi-"
+	export CROSS_COMPILE="/home/cips/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-"
 	#export CFLAGS="-Ofast -mtune=arm1136jf-s -march=armv6j -floop-interchange -floop-strip-mine -fomit-frame-pointer -floop-block -mfpu=vfp -pipe"
 	echo " done."
 
