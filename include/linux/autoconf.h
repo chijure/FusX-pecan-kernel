9295d6958d49c2d79e57640f82783794ac171e10
Ron <cips173@gmail.com>
2012-06-06 11:52:27 +0530

    fix

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59-RM-32-PECAN
- * Tue Jun  5 20:55:39 2012
+ * Wed Jun  6 11:47:38 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
