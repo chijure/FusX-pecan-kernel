0c618ef89d3454194132232e30e599abf0629057
Ron <cips173@gmail.com>
2012-06-06 12:03:00 +0530

    update video decoder to latest versions

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59-RM-32-PECAN
- * Wed Jun  6 11:47:38 2012
+ * Wed Jun  6 12:01:32 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
