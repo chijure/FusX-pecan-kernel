27a5f728124e90db3f74db05d65595e655f319d0
Ron <cips173@gmail.com>
2012-06-19 01:36:46 +0530

    wifi bug testing

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59-RM-32-PECAN
- * Mon Jun 18 20:18:07 2012
+ * Tue Jun 19 01:18:42 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
