6f4a2063dd308ff50d181a64e6b109425463fb87
Ron <cips173@gmail.com>
2012-06-22 17:52:35 +0530

    msm: kgsl: mark GPU wait time as IO time This change, coupled with marking IO time as busy time for cpufreq purposes, will improve performance for GPU bound graphics applications.

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59-RM-32-PECAN
- * Fri Jun 22 15:33:15 2012
+ * Fri Jun 22 17:41:49 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
