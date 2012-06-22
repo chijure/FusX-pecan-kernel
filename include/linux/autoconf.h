0652d2f61f39f7a5c18ce1e16307d3422a761798
Ron <cips173@gmail.com>
2012-06-23 01:35:16 +0530

    Makefile with some Arm6 mods https://github.com/fserve/goldenleaf/commit/780fa9a65898943e0fe4e77ed18a92e58de5c597

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59-RM-32-PECAN
- * Fri Jun 22 17:41:49 2012
+ * Sat Jun 23 01:16:13 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
