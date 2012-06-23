dec4fd869125151a3ab03c5a1780686cda9e5793
Ron <cips173@gmail.com>
2012-06-24 00:38:22 +0530

    ADD: Speed tweak in jhash  by LorDClockaN

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59
- * Sat Jun 23 23:41:02 2012
+ * Sun Jun 24 00:32:15 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
