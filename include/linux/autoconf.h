dd033609c3d9d31e665a11a8cd14cf0ef8a4b894
Ron <cips173@gmail.com>
2012-06-24 13:56:20 +0530

    changes in cpu freq

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59
- * Sun Jun 24 00:32:15 2012
+ * Sun Jun 24 13:47:47 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
