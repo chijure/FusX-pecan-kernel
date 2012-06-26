a8ec77a192b3ed639ebd500c2ea0ed63d82cbdb7
Ron <cips173@gmail.com>
2012-06-26 16:11:34 +0530

    update touch screen driver

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59
- * Tue Jun 26 12:19:57 2012
+ * Tue Jun 26 14:35:37 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
