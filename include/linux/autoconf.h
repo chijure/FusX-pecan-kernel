e8c26357b2500ad26315daf3e4fd1255ad3af505
Ron <cips173@gmail.com>
2012-07-07 17:00:14 +0530

    rc9 release

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59
- * Sat Jul  7 16:12:42 2012
+ * Sat Jul  7 16:56:37 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
