39606137fe4af7fa5be96339743a8deb55ef8ddf
Ron <cips173@gmail.com>
2012-07-22 12:05:49 +0530

    testing

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.59
- * Fri Jul 20 14:58:48 2012
+ * Sun Jul 22 12:04:15 2012
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
